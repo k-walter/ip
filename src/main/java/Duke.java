3761bd44f63c437239f3096198960a2d7ce1f6c8
Walter Kong <walter.98.kong@gmail.com>
2020-08-28 17:29:49 +0800

    Add input scanner with Greet, Echo, Bye commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String greet = "    ____________________________________________________________\n" +
+            "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n" +
+            "    ____________________________________________________________\n";
+
+    private static String bye = "    ____________________________________________________________\n" +
+            "     Bye. Hope to see you again soon!\n" +
+            "    ____________________________________________________________\n";
+
+    private static String echo = "    ____________________________________________________________\n" +
+            "     %s\n" +
+            "    ____________________________________________________________\n";
+
     public static void main(String[] args) {
-        String greet = "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
-        System.out.println(greet);
+        Scanner scan = new Scanner(System.in);
+        String line = "greet";
+
+        do {
+           switch (line) {
+               case "greet":
+                   System.out.println(greet);
+                   break;
+               default:
+                   System.out.println(String.format(echo, line));
+           }
+
+           line = scan.nextLine();
+        } while (!line.equals("bye")) {
+
+        System.out.println(bye);
     }
 }
