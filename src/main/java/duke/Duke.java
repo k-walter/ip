8b8701a0bc7454ffeeccf931a97d93bf820737c8
Walter Kong <walter.98.kong@gmail.com>
2020-09-10 16:59:17 +0800

    Moved task package under duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,9 +1,9 @@
 package duke;
 
-import task.Deadline;
-import task.Event;
-import task.Task;
-import task.Todo;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
