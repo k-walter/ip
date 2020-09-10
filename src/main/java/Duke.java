92e519566701e20b830f2a11f682aa2416987c2a
Walter Kong <walter.98.kong@gmail.com>
2020-09-10 16:51:14 +0800

    Add exception handling by refactoring to separate method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,101 +4,108 @@ import java.util.Scanner;
 
 public class Duke {
     protected static final String HR = "    ____________________________________________________________\n";
-    protected static final String GREET = HR +
-            "     Hello! I'm Duke\n" +
-            "     What can I do for you?\n" +
-            HR;
-    protected static final String BYE = HR +
-            "     Bye. Hope to see you again soon!\n" +
-            HR;
-    protected static final String ECHO = HR +
-            "     %s\n" +
-            HR;
-    protected static final String ADD = HR +
-            "     Got it. I've added this task: \n" +
+    protected static final String GREET = "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n";
+    protected static final String BYE = "     Bye. Hope to see you again soon!\n";
+    protected static final String ECHO = "     %s\n";
+    protected static final String ADD = "     Got it. I've added this task: \n" +
             "       %s\n" +
-            "     Now you have %d tasks in the list.\n" +
-            HR;
-    protected static final String TASK = "     %d. %s";
-    protected static final String DONE = HR +
-            "     Nice! I've marked this task as done: \n" +
-            "       %s\n" +
-            HR;
+            "     Now you have %d tasks in the list.\n";
+    protected static final String TASK = "     %d. %s\n";
+    protected static final String DONE = "     Nice! I've marked this task as done: \n" +
+            "       %s\n";
 
     protected static ArrayList<Task> tasks = new ArrayList<>(100);
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String line = "greet";
+        String stdIn = "greet";
+        String stdOut = "";
 
         do {
-            // Take commands from first space-separated word
-            String[] cmd = line.split(" ");
-            switch (cmd[0]) {
-
-            case "greet": {
-                System.out.println(GREET);
-                break;
+            try {
+                stdOut = parseCmd(stdIn);
+            } catch (UnknownArgumentException e) {
+                stdOut = e.toString();
+            } catch (EmptyArgumentException e) {
+                stdOut = e.toString();
+            } catch (NumberFormatException e) {
+                stdOut = e.toString();
+            } catch (IndexOutOfBoundsException e) {
+                stdOut = e.toString();
+            } catch (Exception e) {
+                stdOut = e.toString();
+            } finally {
+                System.out.println(HR + stdOut + HR);
             }
 
-            case "echo": {
-                String statement = joinUntil(cmd, 1, cmd.length);
-                System.out.println(String.format(ECHO, statement));
-                break;
-            }
-
-            case "list": {
-                System.out.print(HR + "     Here are the tasks in your list:\n");
-                for (int i = 1; i <= tasks.size(); ++i) {
-                    System.out.println(String.format(TASK, i, tasks.get(i - 1)));
-                }
-                System.out.println(HR);
-                break;
-            }
-
-            case "done": {
-                int i = Integer.parseInt(cmd[1]);
-                tasks.get(i - 1).markAsDone();
-                System.out.println(String.format(DONE, tasks.get(i - 1)));
-                break;
-            }
-
-            case "todo": {
-                String description = joinUntil(cmd, 1, cmd.length);
-                Todo t = new Todo(description);
-                addTask(t);
-                break;
-            }
-
-            case "deadline": {
-                int i = indexOfString(cmd, "/by");
-                String description = joinUntil(cmd, 1, i);
-                String by = joinUntil(cmd, i + 1, cmd.length);
-                Deadline d = new Deadline(description, by);
-                addTask(d);
-                break;
-            }
-
-            case "event": {
-                int i = indexOfString(cmd, "/at");
-                String description = joinUntil(cmd, 1, i);
-                String at = joinUntil(cmd, i + 1, cmd.length);
-                Event e = new Event(description, at);
-                addTask(e);
-                break;
-            }
-
-            default: {
-                Task t = new Task(line);
-                addTask(t);
-                break;
-            }
-            }
-
-            line = scan.nextLine();
-        } while (!line.equals("bye"));
+            stdIn = scan.nextLine();
+        } while (!stdIn.equals("bye"));
 
         System.out.println(BYE);
+    }
+
+    protected static String parseCmd(String line) throws EmptyArgumentException, UnknownArgumentException {
+        // Take commands from first space-separated word
+        String[] cmd = line.split(" ");
+        if (cmd.length == 0) {
+            throw new EmptyArgumentException("command");
+        }
+
+        switch (cmd[0]) {
+
+        case "greet": {
+            return GREET;
+        }
+
+        case "echo": {
+            String statement = joinUntil(cmd, 1, cmd.length);
+            return String.format(ECHO, statement);
+        }
+
+        case "list": {
+            StringBuilder out = new StringBuilder("     Here are the tasks in your list:\n");
+            for (int i = 1; i <= tasks.size(); ++i) {
+                out.append(String.format(TASK, i, tasks.get(i - 1)));
+            }
+            return out.toString();
+        }
+
+        case "done": {
+            int i = Integer.parseInt(cmd[1]);
+            tasks.get(i - 1).markAsDone();
+            return String.format(DONE, tasks.get(i - 1));
+        }
+
+        case "todo": {
+            if (cmd.length <= 1) {
+                throw new EmptyArgumentException("todo");
+            }
+            String description = joinUntil(cmd, 1, cmd.length);
+            Todo t = new Todo(description);
+            return addTask(t);
+        }
+
+        case "deadline": {
+            int i = indexOfString(cmd, "/by");
+            String description = joinUntil(cmd, 1, i);
+            String by = joinUntil(cmd, i + 1, cmd.length);
+            Deadline d = new Deadline(description, by);
+            return addTask(d);
+        }
+
+        case "event": {
+            int i = indexOfString(cmd, "/at");
+            String description = joinUntil(cmd, 1, i);
+            String at = joinUntil(cmd, i + 1, cmd.length);
+            Event e = new Event(description, at);
+            return addTask(e);
+        }
+
+        default: {
+            throw new UnknownArgumentException();
+        }
+        }
     }
 
     /**
@@ -106,9 +113,9 @@ public class Duke {
      *
      * @param task - to be added
      */
-    public static void addTask(Task task) {
+    public static String addTask(Task task) {
         tasks.add(task);
-        System.out.println(String.format(ADD, task, tasks.size()));
+        return String.format(ADD, task, tasks.size());
     }
 
     /**
