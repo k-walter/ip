ace3adf24af38fc516844b9bc52ae4b6e2bae8da
Walter Kong <walter.98.kong@gmail.com>
2020-09-10 16:50:20 +0800

    Add exceptions

---
+++ src/main/java/EmptyArgumentException.java
@@ -0,0 +1,19 @@
+public class EmptyArgumentException extends Exception {
+    String descriptor;
+
+    public EmptyArgumentException() {
+        this("");
+    }
+
+    public EmptyArgumentException(String descriptor) {
+        this.descriptor = descriptor;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "     ☹ OOPS!!! The description of a %s cannot be empty.\n",
+                this.descriptor
+        );
+    }
+}
